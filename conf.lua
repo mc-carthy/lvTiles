1cbecb659ff2f2e3238aa1851812f3d6a8ae5a6d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-10 23:33:37 +0100

    Drawing basic tiles

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-    t.window.width = 800
-    t.window.height = 600
+    t.window.width = 400
+    t.window.height = 400
     t.window.title = 'LÖVE Tiles'
     t.window.icon = nil
 
--- main.lua
+++ main.lua
@@ -1,11 +1,5 @@
---[[
-
-    rm -rf .git
-
-]]--
-
 function love.load()
-
+    love.graphics.setNewFont(30)
 end
 
 function love.update(dt)
@@ -13,7 +7,26 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    for y = 1, 4 do
+        for x = 1, 4 do
+            local pieceSize = 100
+            local pieceDrawSize = pieceSize - 1
+            love.graphics.setColor(.4, .1, .6)
+            love.graphics.rectangle(
+                'fill',
+                (x - 1) * pieceSize,
+                (y - 1) * pieceSize,
+                pieceDrawSize,
+                pieceDrawSize
+            )
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.print(
+                x + ((y - 1) * 4),
+                (x - 1) * pieceSize,
+                (y - 1) * pieceSize
+            )
+        end
+    end
 end
 
 function love.keypressed(key)
