b8281ae036a43cc82762e1e6b707b627e2cd571c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-10 23:31:51 +0100

    Init repo

---
+++ main.lua
@@ -0,0 +1,23 @@
+--[[
+
+    rm -rf .git
+
+]]--
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
