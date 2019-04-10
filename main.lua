51811caf53d3b2b5ec5e4206162e487e49c97881
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-10 23:35:32 +0100

    Omit empty tile from drawing

--- main.lua
+++ main.lua
@@ -1,5 +1,16 @@
 function love.load()
     love.graphics.setNewFont(30)
+
+    gridXCount = 4
+    gridYCount = 4
+
+    grid = {}
+    for y = 1, gridYCount do
+        grid[y] = {}
+        for x = 1, gridXCount do
+            grid[y][x] = x + ((y - 1) * gridXCount)
+        end
+    end
 end
 
 function love.update(dt)
@@ -7,24 +18,26 @@ function love.update(dt)
 end
 
 function love.draw()
-    for y = 1, 4 do
-        for x = 1, 4 do
-            local pieceSize = 100
-            local pieceDrawSize = pieceSize - 1
-            love.graphics.setColor(.4, .1, .6)
-            love.graphics.rectangle(
-                'fill',
-                (x - 1) * pieceSize,
-                (y - 1) * pieceSize,
-                pieceDrawSize,
-                pieceDrawSize
-            )
-            love.graphics.setColor(1, 1, 1)
-            love.graphics.print(
-                x + ((y - 1) * 4),
-                (x - 1) * pieceSize,
-                (y - 1) * pieceSize
-            )
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
+            if grid[y][x] ~= gridXCount * gridYCount then
+                local pieceSize = 100
+                local pieceDrawSize = pieceSize - 1
+                love.graphics.setColor(.4, .1, .6)
+                love.graphics.rectangle(
+                    'fill',
+                    (x - 1) * pieceSize,
+                    (y - 1) * pieceSize,
+                    pieceDrawSize,
+                    pieceDrawSize
+                )
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.print(
+                    grid[y][x],
+                    (x - 1) * pieceSize,
+                    (y - 1) * pieceSize
+                )
+            end
         end
     end
 end
@@ -33,4 +46,5 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+
 end
